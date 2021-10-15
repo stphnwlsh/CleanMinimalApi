0a4bf9f89d17505923f0207566ccfd0b48788397
Stephen Walsh <868017+stphnwlsh@users.noreply.github.com>
2021-10-15 11:07:04 +1000

    Cleanup and Add Versions

---
+++ src/Application/Versions/ApiVersion/ApiVersionQuery.cs
@@ -0,0 +1,8 @@
+namespace CleanMinimalApi.Application.Versions.ApiVersion;
+
+using CleanMinimalApi.Domain.Entities.Versions;
+using MediatR;
+
+public class ApiVersionQuery : IRequest<ApiVersion>
+{
+}
