6824dc296a84607ebe5d62748d106a27c043bd4e
Stephen Walsh <868017+stphnwlsh@users.noreply.github.com>
2023-08-20 11:12:54 -0500

    Update and Delete Test Coverage

--- tests/Presentation.Tests.Unit/Endpoints/ReviewEndpointTests.cs
+++ tests/Presentation.Tests.Unit/Endpoints/ReviewEndpointTests.cs
@@ -312,115 +312,148 @@ public class ReviewEndpointTests
         result.ProblemDetails.Status.ShouldBe(StatusCodes.Status500InternalServerError);
         result.ProblemDetails.Detail.ShouldNotBeNullOrEmpty();
     }
+
+    [Fact]
+    public async Task UpdateReview_ShouldReturn_NoContent()
+    {
+        // Arrange
+        var mediator = Substitute.For<IMediator>();
+        var request = new Requests.UpdateReviewRequest
+        {
+            AuthorId = Guid.Empty,
+            MovieId = Guid.Empty,
+            Stars = 5
+        };
+
+        _ = mediator
+            .Send(Arg.Any<Commands.UpdateReview.UpdateReviewCommand>())
+            .ReturnsForAnyArgs(true);
+
+        // Act
+        var response = await ReviewsEndpoints.UpdateReview(Guid.NewGuid(), request, mediator);
+
+        // Assert
+        var result = response.ShouldBeOfType<NoContent>();
+
+        result.StatusCode.ShouldBe(StatusCodes.Status204NoContent);
+    }
+
+    [Fact]
+    public async Task UpdateReview_ShouldReturn_NotFound()
+    {
+        // Arrange
+        var mediator = Substitute.For<IMediator>();
+        var request = new Requests.UpdateReviewRequest
+        {
+            AuthorId = Guid.Empty,
+            MovieId = Guid.Empty,
+            Stars = 5
+        };
+
+        _ = mediator
+            .Send(Arg.Any<Commands.UpdateReview.UpdateReviewCommand>())
+            .Throws(new NotFoundException("Expected Exception"));
+
+        // Act
+        var response = await ReviewsEndpoints.UpdateReview(Guid.NewGuid(), request, mediator);
+
+        // Assert
+        var result = response.ShouldBeOfType<NotFound<string>>();
+
+        result.StatusCode.ShouldBe(StatusCodes.Status404NotFound);
+        result.Value.ShouldBe("Expected Exception");
+    }
+
+    [Fact]
+    public async Task UpdateReview_ShouldReturn_Problem()
+    {
+        // Arrange
+        var mediator = Substitute.For<IMediator>();
+        var request = new Requests.UpdateReviewRequest
+        {
+            AuthorId = Guid.Empty,
+            MovieId = Guid.Empty,
+            Stars = 5
+        };
+
+        _ = mediator
+            .Send(Arg.Any<Commands.UpdateReview.UpdateReviewCommand>())
+            .Throws(new ArgumentException("Expected Exception"));
+
+        // Act
+        var response = await ReviewsEndpoints.UpdateReview(Guid.NewGuid(), request, mediator);
+
+        // Assert
+        var result = response.ShouldBeOfType<ProblemHttpResult>();
+
+        result.StatusCode.ShouldBe(StatusCodes.Status500InternalServerError);
+
+        result.ProblemDetails.Title.ShouldBe("An error occurred while processing your request.");
+        result.ProblemDetails.Instance.ShouldBe("Expected Exception");
+        result.ProblemDetails.Status.ShouldBe(StatusCodes.Status500InternalServerError);
+        result.ProblemDetails.Detail.ShouldNotBeNullOrEmpty();
+    }
+
+    [Fact]
+    public async Task DeleteReview_ShouldReturn_NoContent()
+    {
+        // Arrange
+        var mediator = Substitute.For<IMediator>();
+
+        _ = mediator
+            .Send(Arg.Any<Commands.DeleteReview.DeleteReviewCommand>())
+            .ReturnsForAnyArgs(true);
+
+        // Act
+        var response = await ReviewsEndpoints.DeleteReview(Guid.NewGuid(), mediator);
+
+        // Assert
+        var result = response.ShouldBeOfType<NoContent>();
+
+        result.StatusCode.ShouldBe(StatusCodes.Status204NoContent);
+    }
+
+    [Fact]
+    public async Task DeleteReview_ShouldReturn_NotFound()
+    {
+        // Arrange
+        var mediator = Substitute.For<IMediator>();
+
+        _ = mediator
+            .Send(Arg.Any<Commands.DeleteReview.DeleteReviewCommand>())
+            .Throws(new NotFoundException("Expected Exception"));
+
+        // Act
+        var response = await ReviewsEndpoints.DeleteReview(Guid.NewGuid(), mediator);
+
+        // Assert
+        var result = response.ShouldBeOfType<NotFound<string>>();
+
+        result.StatusCode.ShouldBe(StatusCodes.Status404NotFound);
+        result.Value.ShouldBe("Expected Exception");
+    }
+
+    [Fact]
+    public async Task DeleteReview_ShouldReturn_Problem()
+    {
+        // Arrange
+        var mediator = Substitute.For<IMediator>();
+
+        _ = mediator
+            .Send(Arg.Any<Commands.DeleteReview.DeleteReviewCommand>())
+            .Throws(new ArgumentException("Expected Exception"));
+
+        // Act
+        var response = await ReviewsEndpoints.DeleteReview(Guid.NewGuid(), mediator);
+
+        // Assert
+        var result = response.ShouldBeOfType<ProblemHttpResult>();
+
+        result.StatusCode.ShouldBe(StatusCodes.Status500InternalServerError);
+
+        result.ProblemDetails.Title.ShouldBe("An error occurred while processing your request.");
+        result.ProblemDetails.Instance.ShouldBe("Expected Exception");
+        result.ProblemDetails.Status.ShouldBe(StatusCodes.Status500InternalServerError);
+        result.ProblemDetails.Detail.ShouldNotBeNullOrEmpty();
+    }
 }
-
-
-// namespace CleanMinimalApi.Presentation.Tests.Unit.Endpoints;
-
-// using System.Collections.Generic;
-// using System.Net;
-// using System.Net.Http;
-// using System.Text;
-// using System.Threading.Tasks;
-// using Application.Authors.Entities;
-// using Application.Reviews.Entities;
-// using Application.Reviews.Entities;
-// using Extensions;
-// using Shouldly;
-// using Xunit;
-
-// public class ReviewEndpointTests
-// {
-//     private static readonly MinimalApiApplication Application = new();
-
-//     [Fact]
-//     public async Task CreateReview_ShouldReturn_Created()
-//     {
-//         // Arrange
-//         using var client = Application.CreateClient();
-
-//         using var authorResponse = await client.GetAsync("/api/authors");
-//         var authorResult = (await authorResponse.Content.ReadAsStringAsync()).Deserialize<List<Author>>()[0];
-//         using var movieResponse = await client.GetAsync("/api/movies");
-//         var movieResult = (await movieResponse.Content.ReadAsStringAsync()).Deserialize<List<Review>>()[0];
-//         var json = (new { Stars = 5, AuthorId = authorResult.Id, ReviewId = movieResult.Id }).Serialize();
-//         var content = new StringContent(json, Encoding.UTF8, "application/json");
-
-//         // Act
-//         using var response = await client.PostAsync("/api/reviews", content);
-//         var result = (await response.Content.ReadAsStringAsync()).Deserialize<Review>();
-
-//         // Assert
-//         response.StatusCode.ShouldBe(HttpStatusCode.Created);
-
-//         _ = result.ShouldNotBeNull();
-
-//         _ = result.Id.ShouldBeOfType<Guid>();
-//         _ = result.Stars.ShouldBeOfType<int>();
-//         result.Stars.ShouldBe(5);
-
-//         _ = result.ReviewAuthor.ShouldNotBeNull();
-//         result.ReviewAuthor.Id.ShouldBe(authorResult.Id);
-//         result.ReviewAuthor.FirstName.ShouldBe(authorResult.FirstName);
-//         result.ReviewAuthor.LastName.ShouldBe(authorResult.LastName);
-
-//         _ = result.ReviewedReview.ShouldNotBeNull();
-//         result.ReviewedReview.Id.ShouldBe(movieResult.Id);
-//         result.ReviewedReview.Title.ShouldBe(movieResult.Title);
-//     }
-
-//     [Fact]
-//     public async Task DeleteReview_ShouldDelete_Review()
-//     {
-//         // Arrange
-//         using var client = Application.CreateClient();
-//         using var reviewResponse = await client.GetAsync("/api/reviews");
-//         var reviewResult = (await reviewResponse.Content.ReadAsStringAsync()).Deserialize<List<Review>>()[0];
-
-//         // Act
-//         using var response = await client.DeleteAsync($"/api/reviews/{reviewResult.Id}");
-
-//         // Assert
-//         response.StatusCode.ShouldBe(HttpStatusCode.NoContent);
-
-//         (await client.GetAsync($"/api/reviews/{reviewResult.Id}")).StatusCode.ShouldBe(HttpStatusCode.NotFound);
-//     }
-
-//     [Fact]
-//     public async Task UpdateReview_ShouldReturn_NoContent()
-//     {
-//         // Arrange
-//         using var client = Application.CreateClient();
-//         using var authorResponse = await client.GetAsync("/api/authors");
-//         var authorResult = (await authorResponse.Content.ReadAsStringAsync()).Deserialize<List<Author>>()[0];
-//         using var movieResponse = await client.GetAsync("/api/movies");
-//         var movieResult = (await movieResponse.Content.ReadAsStringAsync()).Deserialize<List<Review>>()[0];
-//         using var reviewResponse = await client.GetAsync("/api/reviews");
-//         var reviewResult = (await reviewResponse.Content.ReadAsStringAsync()).Deserialize<List<Review>>()[0];
-//         var json = (new { Stars = 5, AuthorId = authorResult.Id, ReviewId = movieResult.Id }).Serialize();
-//         var content = new StringContent(json, Encoding.UTF8, "application/json");
-
-//         // Act
-//         using var response = await client.PutAsync($"/api/reviews/{reviewResult.Id}", content);
-
-//         // Assert
-//         response.StatusCode.ShouldBe(HttpStatusCode.NoContent);
-
-//         using var validateResponse = await client.GetAsync($"/api/reviews/{reviewResult.Id}");
-//         var validateResult = (await validateResponse.Content.ReadAsStringAsync()).Deserialize<Review>();
-
-//         _ = validateResult.ShouldNotBeNull();
-
-//         validateResult.Id.ShouldBe(reviewResult.Id);
-//         validateResult.Stars.ShouldBe(5);
-
-//         _ = validateResult.ReviewAuthor.ShouldNotBeNull();
-//         validateResult.ReviewAuthor.Id.ShouldBe(authorResult.Id);
-//         validateResult.ReviewAuthor.FirstName.ShouldBe(authorResult.FirstName);
-//         validateResult.ReviewAuthor.LastName.ShouldBe(authorResult.LastName);
-
-//         _ = validateResult.ReviewedReview.ShouldNotBeNull();
-//         validateResult.ReviewedReview.Id.ShouldBe(movieResult.Id);
-//         validateResult.ReviewedReview.Title.ShouldBe(movieResult.Title);
-//     }
-// }
